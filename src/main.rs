575820be83ea8bab417396795c4e868bde33231a
Jessica Stokes <hello@jessicastokes.net>
2024-09-14 10:29:30 -0700

    Sketched up to the point of finding all the entries matching taggings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use clap::Parser;
-use feedbin_api::models::CacheRequestResponse;
+use feedbin_api::models::{CacheRequestResponse, Entry};
 use feedbin_api::FeedbinApi;
 use lazy_static::lazy_static;
-use std::collections::HashMap;
 use url::Url;
 
 lazy_static! {
@@ -21,6 +20,10 @@ struct Args {
     /// Your Feedbin password
     #[arg(short, long)]
     password: String,
+
+    /// Names of tags to clean up feeds from
+    #[arg(short, long, required = true)]
+    tagged: Vec<String>,
 }
 
 #[tokio::main]
@@ -33,31 +36,70 @@ async fn main() {
     let api = FeedbinApi::new(&FEEDBIN_BASE_URL, args.username, args.password);
     eprintln!("Created API client");
 
-    let mut tag_map: HashMap<String, Vec<u64>> = HashMap::new();
+    let mut target_feed_ids: Vec<u64> = Vec::new();
 
     {
         let taggings_response = api.get_taggings(&client, None).await.unwrap();
+
         eprintln!("Taggings requested");
 
         if let CacheRequestResponse::Modified(taggings) = taggings_response {
+            // TODO: Error if no matches found
             for tagging in &taggings.value {
-                if tag_map.contains_key(&tagging.name) {
-                    let feed_list = tag_map
-                        .get_mut(&tagging.name)
-                        .expect("Could not get tagging which should have existed");
-                    feed_list.push(tagging.feed_id);
-                } else {
-                    tag_map.insert(tagging.name.clone(), vec![tagging.feed_id]);
+                // TODO: Should this be a glob match, maybe?
+                if args.tagged.contains(&tagging.name) {
+                    target_feed_ids.push(tagging.feed_id);
                 }
             }
         }
     };
 
-    for (key, val) in tag_map.iter() {
-        println!("Tagging \"{}\" contains {} feed(s):", key, val.len());
+    // Sort the IDs so that we can `binary_search` it later
+    target_feed_ids.sort();
 
-        for feed_id in val.iter() {
-            println!(" - {}", feed_id);
-        }
+    for feed_id in target_feed_ids.iter() {
+        eprintln!(" - {}", feed_id);
     }
+
+    eprintln!("Getting unread entry IDs");
+    let all_unread_entry_ids = api.get_unread_entry_ids(&client).await.unwrap();
+    eprintln!("There are {} unread entries", all_unread_entry_ids.len());
+
+    eprintln!("Getting entries for above feed IDs");
+
+    let mut tagged_unread_entries: Vec<Entry> = Vec::new();
+
+    // We can only request 100 entries per page, so we chunk the above list
+    for (index, unread_entry_page) in all_unread_entry_ids.chunks(100).enumerate() {
+        let mut entries_response = api
+            .get_entries(
+                &client,
+                None,
+                None,
+                Some(unread_entry_page),
+                None,
+                None,
+                false,
+            )
+            .await
+            .unwrap();
+
+        let total_page_entries = entries_response.len();
+
+        entries_response.retain(|entry| target_feed_ids.binary_search(&entry.feed_id).is_ok());
+
+        eprintln!(
+            "Tagged entries in page {}: {}/{}",
+            index,
+            entries_response.len(),
+            total_page_entries
+        );
+
+        tagged_unread_entries.append(&mut entries_response);
+    }
+
+    eprintln!("Tagged Entries: {:#?}", tagged_unread_entries);
+
+    // There is a limit of 1,000 entry_ids per request
+    // api.set_entries_read(&client, matching_entries).await.unwrap();
 }
